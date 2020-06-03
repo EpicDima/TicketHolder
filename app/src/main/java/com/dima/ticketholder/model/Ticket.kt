2b529d928e0dd0e72b01c354f54a5a53586ef03e
Юрий Сергеевич Башаримов <33197420+UrilKxx@users.noreply.github.com>
2020-06-03 09:58:12 +0300

    Исправлены  ошибки по части стиля кода.
    Fixed error on the part of the code style.

--- app/src/main/java/com/dima/ticketholder/model/Ticket.kt
+++ app/src/main/java/com/dima/ticketholder/model/Ticket.kt
@@ -82,27 +82,22 @@ data class Ticket(
         var part = thisPart
         val sameSize = thisWidth == anotherWidth && thisHeight == anotherHeight
         val sameSizeAfterRotate = thisWidth == anotherHeight && thisHeight == anotherWidth
+        var result = false
 
-        fun do_compare(part: Array<BooleanArray>) : Boolean  {
-
+        fun doCompare(part: Array<BooleanArray>) : Boolean  {
             val h = horizontalReflection(part)
             val v = verticalReflection(part)
             val hv = verticalReflection(h)
-
             return part.contentDeepEquals(anotherPart)
                     || h.contentDeepEquals(anotherPart)
                     || v.contentDeepEquals(anotherPart)
                     || hv.contentDeepEquals(anotherPart)
         }
-        var result = false
-
         if (sameSize || sameSizeAfterRotate) {
-
-            result = do_compare(part)
-
+            result = doCompare(part)
             if (!result && sameSizeAfterRotate) {
                 part = rotate(part)
-                result = do_compare(part)
+                result = doCompare(part)
             }
         }
         return result
